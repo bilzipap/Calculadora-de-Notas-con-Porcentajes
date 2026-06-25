1ebd13e9f237da9eefa53d75e8e59f4a0f50f16b
Benjamín <alarconbenjamin37@gmail.com>
2026-06-24 21:43:17 -0400

    nuevas funciones

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 notas = []
 porcentajes = []
 
-def calcular_nota(notas, porcentajes):
-    if len(notas) != len(porcentajes):
+def calcular_nota(notasl, porcentajesl):
+    if len(notasl) != len(porcentajesl):
         return "El largo de notas no coincide al de porcentajes!!"
     total = 0
-    for nota, porcentaje in zip(notas, porcentajes):
-        total += nota*porcentaje
+    for n, p in zip(notasl, porcentajesl):
+        total += n*p
     return total
 
 while True:
@@ -18,7 +18,8 @@ while True:
     | 1. Agregar una nota         |
     | 2. Configurar porcentajes   | 
     | 3. Calcular notas           |
-    | 4. Salir                    |  
+    | 4. Cuanto necesitas         |
+    | 5. Salir                    |  
     |                             |
     *-----------------------------*
     """)
@@ -52,7 +53,32 @@ while True:
             for nota, porcentaje in zip(notas, porcentajes):
                 count += 1
                 print(f'{count}. {nota}: {porcentaje}%')
-
             time.sleep(3)
         case 4:
+            nacumulado = sum(n*p for n, p in zip(notas, porcentajes))
+
+            notadeseada = int(input("Ingrese la nota deseada: "))
+            pfaltante = 0
+
+            for p in porcentajes:
+                pfaltante += p
+
+            if pfaltante <= 0:
+                print("Porcentaje máximo ya alcanzado(100%)")
+
+            pacumulado = 1 - pfaltante
+            puntosfaltantes = notadeseada - nacumulado
+            notanecesaria = puntosfaltantes / pfaltante
+
+            print(f"\n--- Resultados para nota objetivo: {notadeseada} ---")
+
+            if notanecesaria <= 1.0:
+                print("¡Ya aprobaste! Incluso sacando un 1.0 en todo lo que falta alcanzas tu meta.")
+            elif notanecesaria > 7.0:
+                print(f"Uf, matemáticamente imposible. Necesitarías promediar un {notanecesaria:.2f} en lo que falta.")
+            else:
+                print(
+                    f"Necesitas promediar un **{notanecesaria:.2f}** en el {pacumulado * 100:.0f}% restante.")
+            time.sleep(3)
+        case 5:
             break
